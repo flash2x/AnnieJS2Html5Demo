cf604ed75b491a7f8dff094fe4e267588d7fe31e
anlun214 <anlun214@qq.com>
2018-08-06 12:29:14 +0800

    AnnieJS 2.x 示例集

--- source/src/home/DownBtn.js
+++ source/src/home/DownBtn.js
@@ -4,5 +4,8 @@ home.DownBtn=function(){
 	annie.Sprite.call(s);
 	/*_a2x_need_start*//*_a2x_need_end*/
 	annie.initRes(s,"home","DownBtn");
+	s.addEventListener(annie.MouseEvent.CLICK,function (e) {
+		annie.navigateToURL("https://gitee.com/flash2x/AnnieJS2Html5Demo");
+    })
 };
 A2xExtend(home.DownBtn,annie.Sprite);
